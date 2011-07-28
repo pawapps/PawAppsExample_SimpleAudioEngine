9ea9e8ab441020f302040d9aea7149330bd67c19
Jason Pawlak <jason@ender.gateway.2wire.net>
2011-07-28 17:10:24 -0500

    Initial Commit

---
+++ PawAppsExample_SimpleAudioEngine/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  PawAppsExample_SimpleAudioEngine
+//
+//  Created by Jason Pawlak on 7/28/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ PawAppsExample_SimpleAudioEngine/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  PawAppsExample_SimpleAudioEngine
+//
+//  Created by Jason Pawlak on 7/28/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
